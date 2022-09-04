f0f666b2702f4f96c25e56c40b3e5a1f0d3fa635
YankovichKirill <kirill.best97@gmail.com>
2022-09-04 22:35:42 +0300

    Save2

---
+++ Example2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter the number N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+    
+    int SumNumber(int numberN){
+        
+        int counter = Convert.ToString(numberN).Length;
+        int advance = 0;
+        int result = 0;
+
+        for (int i = 0; i < counter; i++){
+            advance = numberN - numberN % 10;
+            result = result + (numberN - advance);
+            numberN = numberN / 10;
+        }
+      return result;
+    }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("The sum of the digits in a number:" + sumNumber);
